b945586fc557f0715e67ef4a22bf45c765c64f37
Ben Collins <fbencollins572@gmail.com>
2026-01-07 10:27:42 +0200

    Setting up project

---
+++ backend/lashreserve/src/main/java/com/lashreserve/lashreserve/LashreserveApplication.java
@@ -0,0 +1,13 @@
+package com.lashreserve.lashreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LashreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LashreserveApplication.class, args);
+	}
+
+}
---
+++ backend/lashreserve/src/test/java/com/lashreserve/lashreserve/LashreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lashreserve.lashreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LashreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
