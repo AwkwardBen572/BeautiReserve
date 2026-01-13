4d0e6814b4efd90d8d4755e0f719596c395ca376
Ben Collins <fbencollins572@gmail.com>
2026-01-13 12:53:11 +0200

    Finished off register functionality and neatened up some code

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -16,6 +16,17 @@ const InputField = ({ label, name, type, value, onChange }) => (
   </div>
 );
 
+const ShowPasswordToggle = ({ showPassword, setShowPassword }) => (
+  <div className="show_password_holder flex_row flex_all_center color_green">
+    <input
+      type="checkbox"
+      checked={showPassword}
+      onChange={() => setShowPassword(!showPassword)}
+    />
+    &nbsp;<div>Show Password</div>
+  </div>
+);
+
 const LoginPage = () => {
   const [mode, setMode] = useState("login");
   const [showPassword, setShowPassword] = useState(false);
@@ -48,217 +59,141 @@ const LoginPage = () => {
         setShowError(true);
         return;
       }
-      var res = await fetch("http://localhost:8080/api/auth/register", {
+
+      const res = await fetch("http://localhost:8080/api/auth/register", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
       });
-      const text = await res.text();
-      setError(text);
 
-    } else {
-      var email = form.email
-      var password = form.password
-      const response = await fetch(
-        "http://localhost:8080/api/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-        }
-      );
-
-      if (response.ok) {
-        console.log("response")
-        console.log(response)
+      if (res.ok) {
+        setMode("login");
       } else {
-        const error = await response.text();
-        console.log("error")
-        console.log(error)
-        setError(error);
+        setError(
+          "There was an error with registering. The user may already exist or details are incorrect."
+        );
+        setShowError(true);
       }
-    };
-};
+    } else {
+      const { email, password } = form;
+      const res = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ email, password })
+      });
 
-const validateForm = () => {
-  let errors = [];
-  if (!form.fullName.trim()) errors.push("Please insert your name & surname.");
-  if (
-    !form.phoneNumber.trim() ||
-    form.phoneNumber.length < 10 ||
-    !validatePhone(form.phoneNumber)
-  ) errors.push("Please insert a valid phone number.");
-  if (!validateEmail(form.email)) errors.push("Please insert a valid email.");
-  if (form.password.length < 8) errors.push("Password must be at least 6 characters.");
-  if (form.password !== form.confirmPassword) errors.push("Passwords do not match.");
-  return errors;
-};
+      if (!res.ok) {
+        setError(
+          "There was an error logging in. The user may not exist or details are incorrect."
+        );
+        setShowError(true);
+      }
+    }
+  };
 
-const validatePhone = (number) => {
-  const cleaned = number.replace(/[\s-()]/g, "");
-  const saPhoneRegex = /^0\d{9}$/;
-  return saPhoneRegex.test(cleaned);
-};
+  const validateForm = () => {
+    const errors = [];
 
-const validateEmail = (email) => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-};
+    if (!form.fullName.trim()) errors.push("Please insert your name & surname.");
+    if (!form.phoneNumber.trim() || form.phoneNumber.length < 10 || !validatePhone(form.phoneNumber))
+      errors.push("Please insert a valid phone number.");
+    if (!validateEmail(form.email)) errors.push("Please insert a valid email.");
+    if (form.password.length < 8) errors.push("Password must be at least 8 characters.");
+    if (form.password !== form.confirmPassword) errors.push("Passwords do not match.");
 
-return (
-  <div className="login_page_holder flex_row font_1">
-    <div className="login_page_bg_holder"></div>
+    return errors;
+  };
 
-    <div className="login_form_holder flex_column flex_all_center">
-      <div className="login_brandname_holder flex_column flex_all_center">
-        <div className="logo_holder"></div>
-        <div className="slogan_holder color_green">
-          <i>Reserve your beauty, effortlessly.</i>
+  const validatePhone = (number) => {
+    const cleaned = number.replace(/[\s-()]/g, "");
+    const saPhoneRegex = /^0\d{9}$/;
+    return saPhoneRegex.test(cleaned);
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  return (
+    <div className="login_page_holder flex_row font_1">
+      <div className="login_page_bg_holder"></div>
+
+      <div className="login_form_holder flex_column flex_all_center">
+        <div className="login_brandname_holder flex_column flex_all_center">
+          <div className="logo_holder"></div>
+          <div className="slogan_holder color_green">
+            <i>Reserve your beauty, effortlessly.</i>
+          </div>
         </div>
-      </div>
 
-      <div className="login_register_option_holder flex_row flex_all_center">
-        <div
-          className={`login_option flex_row flex_all_center ${mode === "login" ? "login_option_selected" : "login_option"
-            }`}
-          onClick={() => setMode("login")}
-        >
-          Login
-        </div>
-        <div
-          className={`login_option flex_row flex_all_center ${mode === "register" ? "login_option_selected" : "login_option"
-            }`}
-          onClick={() => setMode("register")}
-        >
-          Register
-        </div>
-      </div>
-
-      <div className="form_holder">
-        {mode === "login" && (
-          <div className="flex_column flex_all_center">
-            <div className="login_input_holder">
-              <input
-                className="input"
-                type="text"
-                placeholder="Email"
-                name="email"
-                value={form.email}
-                onChange={handleChange}
-              />
+        <div className="login_register_option_holder flex_row flex_all_center">
+          {["login", "register"].map((option) => (
+            <div
+              key={option}
+              className={`login_option flex_row flex_all_center ${
+                mode === option ? "login_option_selected" : "login_option"
+              }`}
+              onClick={() => setMode(option)}
+            >
+              {option.charAt(0).toUpperCase() + option.slice(1)}
             </div>
-            &emsp;
-            <div className="login_input_holder">
-              <input
-                className="input"
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
+          ))}
+        </div>
+
+        <div className="form_holder">
+          {mode === "login" && (
+            <div className="flex_column flex_all_center">
+              <InputField label="Email" name="email" type="text" value={form.email} onChange={handleChange} />
+              &nbsp;
+              <InputField
+                label="Password"
                 name="password"
+                type={showPassword ? "text" : "password"}
                 value={form.password}
                 onChange={handleChange}
               />
-            </div>
-            <div className="show_password_holder flex_row flex_all_center color_green">
-              <input
-                type="checkbox"
-                checked={showPassword}
-                onChange={() => setShowPassword(!showPassword)}
-              />
-              &nbsp;<div className="">Show Password</div>
-            </div>
-            <div className="form_button_holder flex_column flex_all_center">
-              <div className="form_button button flex_column flex_all_center color_white" onClick={handleSubmit}>
-                Login
+              <ShowPasswordToggle showPassword={showPassword} setShowPassword={setShowPassword} />
+              <div className="form_button_holder flex_column flex_all_center">
+                <div className="form_button button flex_column flex_all_center color_white" onClick={handleSubmit}>
+                  Login
+                </div>
+              </div>
+              <div className="forgot_password_holder flex_column flex_all_center color_green">
+                <u style={{ cursor: "pointer" }} onClick={() => navigate("/forgot-password")}>
+                  Forgot Password
+                </u>
               </div>
             </div>
-            <div className="forgot_password_holder flex_column flex_all_center color_green">
-              <u
-                style={{ cursor: "pointer" }}
-                onClick={() => navigate("/forgot-password")}
-              >
-                Forgot Password
-              </u>
-            </div>
-          </div>
-        )}
+          )}
 
-        {mode === "register" && (
-          <div className="flex_column flex_all_center">
-            <InputField
-              label="Name & Surname"
-              name="fullName"
-              type="text"
-              value={form.fullName}
-              onChange={handleChange}
-            />
-            &emsp;
-            <InputField
-              label="Phone Number"
-              name="phoneNumber"
-              type="text"
-              value={form.phoneNumber}
-              onChange={handleChange}
-            />
-            &emsp;
-            <InputField
-              label="Email"
-              name="email"
-              type="text"
-              value={form.email}
-              onChange={handleChange}
-            />
-            &emsp;
-            <InputField
-              label="Password"
-              name="password"
-              type={showPassword ? "text" : "password"}
-              value={form.password}
-              onChange={handleChange}
-            />
-            &emsp;
-            <InputField
-              label="Confirm Password"
-              name="confirmPassword"
-              type={showPassword ? "text" : "password"}
-              value={form.confirmPassword}
-              onChange={handleChange}
-            />
-            <div className="show_password_holder flex_row flex_all_center color_green">
-              <input
-                type="checkbox"
-                checked={showPassword}
-                onChange={() => setShowPassword(!showPassword)}
-              />
-              &nbsp;<div className="">Show Password</div>
-            </div>
-            <div
-              className="form_button_holder flex_column flex_all_center"
-              onClick={handleSubmit}
-            >
-              <div className="form_button button flex_column flex_all_center color_white">
-                Register
+          {mode === "register" && (
+            <div className="flex_column flex_all_center">
+              <InputField label="Name & Surname" name="fullName" type="text" value={form.fullName} onChange={handleChange} />
+              &nbsp;
+              <InputField label="Phone Number" name="phoneNumber" type="text" value={form.phoneNumber} onChange={handleChange} />
+              &nbsp;
+              <InputField label="Email" name="email" type="text" value={form.email} onChange={handleChange} />
+              &nbsp;
+              <InputField label="Password" name="password" type={showPassword ? "text" : "password"} value={form.password} onChange={handleChange} />
+              &nbsp;
+              <InputField label="Confirm Password" name="confirmPassword" type={showPassword ? "text" : "password"} value={form.confirmPassword} onChange={handleChange} />
+              &nbsp;
+              <ShowPasswordToggle showPassword={showPassword} setShowPassword={setShowPassword} />
+              <div className="form_button_holder flex_column flex_all_center">
+                <div className="form_button button flex_column flex_all_center color_white" onClick={handleSubmit}>
+                  Register
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
 
-    <ErrorModal
-      show={showError}
-      message={error}
-      onClose={() => setShowError(false)}
-    />
-  </div>
-);
+      <ErrorModal show={showError} message={error} onClose={() => setShowError(false)} />
+    </div>
+  );
 };
 
 export default LoginPage;
